435868eba528ec41c6a74373f154db2b59c824f0
xiaobxia <673806687@qq.com>
2021-01-29 13:27:43 +0800

    xg

---
+++ src/components/BuyQr/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>1111</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeMain',
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
