28fb3c67a74850a832bc50cffb7e80e58acbe320
xiaobxia <673806687@qq.com>
2021-01-29 11:06:36 +0800

    xg

--- src/util/openCountDown.js
+++ src/util/openCountDown.js
@@ -11,8 +11,27 @@ function marketOpenDayCountDown () {
     const open = (new Date(year, month, day, 9)).getTime()
     return open - d.getTime()
   }
-  if (hour === 16) {
-    const open = (new Date(year, month, day, 17)).getTime()
+  if (hour === 14) {
+    const open = (new Date(year, month, day, 15)).getTime()
+    return open - d.getTime()
+  }
+  if (hour >= 22) {
+    const open = (new Date(year, month, day + 1, 9)).getTime()
+    return open - d.getTime()
+  }
+  return false
+}
+
+function marketCloseDayCountDown () {
+  const d = dateUtil.getDate()
+  const year = d.getFullYear()
+  const month = d.getMonth()
+  const day = d.getDate()
+  const hour = d.getHours()
+  // const minute = d.getMinutes()
+  // const second = d.getSeconds()
+  if (hour < 9) {
+    const open = (new Date(year, month, day, 9)).getTime()
     return open - d.getTime()
   }
   if (hour >= 22) {
@@ -23,5 +42,6 @@ function marketOpenDayCountDown () {
 }
 
 export default {
-  marketOpenDayCountDown
+  marketOpenDayCountDown,
+  marketCloseDayCountDown
 }
