195d9e6562991b2813b212b7e6b19a78aa01274f
xiaobxia <673806687@qq.com>
2021-02-05 13:46:47 +0800

    xg

--- src/routers/Home/Main/index.vue
+++ src/routers/Home/Main/index.vue
@@ -8,15 +8,31 @@
     <div class="h-c">
       <div class="p-t">策略工具</div>
       <div class="e-card c-1"   @click="toToday">
+        <div class="card-title">
+          <div class="m-t">涨跌幅度</div>
+          <div class="d-t">swrwerwsss</div>
+        </div>
         <img src="../../../assets/card/涨幅.png" alt="">
       </div>
       <div class="e-card c-2" @click="openCountDownShow">
+        <div class="card-title">
+          <div class="m-t">风险分布</div>
+          <div class="d-t">swrwerwsss</div>
+        </div>
         <img src="../../../assets/card/分布.png" alt="">
       </div>
       <div class="e-card c-3" @click="toFixBuy">
+        <div class="card-title">
+          <div class="m-t">定投信号</div>
+          <div class="d-t">swrwerwsss</div>
+        </div>
         <img src="../../../assets/card/我的定投.png" alt="">
       </div>
       <div class="e-card c-4" @click="toBuySell">
+        <div class="card-title">
+          <div class="m-t">波段信号</div>
+          <div class="d-t">swrwerwsss</div>
+        </div>
         <img src="../../../assets/card/波段.png" alt="">
       </div>
     </div>
@@ -65,6 +81,9 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
   $original--color-white: #FFFFFF !default;
+  .oo {
+    color: mix($original--color-white, #FFCC00, 40%);
+  }
   .c-d-w {
     text-align: center;
     padding: 24px;
@@ -119,12 +138,12 @@ export default {
       height: 80px;
       width: 80px;
     }
+    color: #fff;
     &.c-1 {
       color: rgb(135, 56, 0);
       background-color: rgb(255, 204, 0);
     }
     &.c-2 {
-      color: mix($original--color-white, #FFCC00, 40%);
       background-color: rgb(255, 102, 52);
     }
     &.c-3 {
@@ -144,6 +163,16 @@ export default {
       box-shadow: 0 0 100px 100px #fff;
       width: 20px;
       height: 20px;
+    }
+    .card-title {
+      margin-top: 10px;
+    }
+    .m-t {
+      font-size: 30px;
+      margin-bottom: 10px;
+    }
+    .d-t {
+
     }
   }
 </style>
