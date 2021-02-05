bdb56180148235c7ec507aeecba95f3bd33db031
xiaobxia <673806687@qq.com>
2021-02-05 13:32:05 +0800

    xg

--- src/routers/Home/Main/index.vue
+++ src/routers/Home/Main/index.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="home-main">
-    <div class="p-t">策略工具</div>
-    <van-button type="primary" @click="openCountDownShow">指数风险</van-button>
-    <van-button type="primary" @click="toToday">今日涨跌幅</van-button>
-    <van-button type="primary" @click="toBuySell">买卖信号</van-button>
-    <van-button type="primary" @click="toFixBuy">定投信号</van-button>
+    <van-notice-bar
+      v-if="buyWarn"
+      left-icon="volume-o"
+      :text="`您的会员还有${userInfo.vip_days}个交易日到期。`"
+    />
+    <div class="h-c">
+      <div class="p-t">策略工具</div>
+      <van-button type="primary" @click="openCountDownShow">指数风险</van-button>
+      <van-button type="primary" @click="toToday">今日涨跌幅</van-button>
+      <van-button type="primary" @click="toBuySell">买卖信号</van-button>
+      <van-button type="primary" @click="toFixBuy">定投信号</van-button>
+      <div class="e-card c-1">
+        <img src="../../../assets/card/涨幅.png" alt="">
+      </div>
+      <div class="e-card c-2">
+        <img src="../../../assets/card/分布.png" alt="">
+      </div>
+      <div class="e-card c-3">
+        <img src="../../../assets/card/我的定投.png" alt="">
+      </div>
+      <div class="e-card c-4">
+        <img src="../../../assets/card/波段.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'HomeMain',
   data () {
     return {
       countDownShow: false,
       time: 0
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'userInfo'
+    ]),
+    buyWarn () {
+      return this.userInfo.vip_days === 1 || this.userInfo.vip_days === 2
     }
   },
   created () {
@@ -40,6 +68,7 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+  $original--color-white: #FFFFFF !default;
   .c-d-w {
     text-align: center;
     padding: 24px;
@@ -67,9 +96,57 @@ export default {
     }
   }
   .home-main {
-    padding: 10px 10px;
+    .h-c {
+      padding: 10px 10px;
+      padding-bottom: 100px;
+    }
   }
   .p-t {
     font-size: 26px;
+  }
+  .e-card {
+    width: 100%;
+    border-radius: 8px;
+    height: 120px;
+    position: relative;
+    margin: 12px 0;
+    overflow: hidden;
+    padding: 20px;
+    box-sizing: border-box;
+    img {
+      position: absolute;
+      display: block;
+      top: 0;
+      bottom: 0;
+      margin: auto;
+      right: 20px;
+      height: 80px;
+      width: 80px;
+    }
+    &.c-1 {
+      color: mix($original--color-white, #FFCC00, 40%);
+      background-color: rgb(255, 204, 0);
+    }
+    &.c-2 {
+      background-color: rgb(255, 102, 52);
+    }
+    &.c-3 {
+      background-color: rgb(63, 162, 255);
+    }
+    &.c-4 {
+      background-color: rgb(166, 77, 255);
+    }
+    &:after {
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+      bottom: -140px;
+      content: '';
+      display: block;
+      box-shadow: 0 0 100px 100px #fff;
+      width: 20px;
+      height: 20px;
+    }
   }
 </style>
